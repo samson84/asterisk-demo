4ab0d7165073580684ef19e6ea2f2cd87fc8a45f
samson84 <samson84@gmail.com>
2017-02-03 13:03:50 +0100

    docker-build dir corrected

---
+++ docker-build/asterisk-docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Process templates
+find /etc/asterisk -name '*.j2' | while read template; do
+    # replace templated values with environment variables of the same name
+    j2 ${template} > /etc/asterisk/$(basename ${template} .j2)
+    # remove the template once processed
+    rm -f ${template}
+done
+
+echo env
+
+exec "$@"
